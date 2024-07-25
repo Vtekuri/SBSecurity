e7e8cc10311934ce67f2b103a318a634669d58e6
91967 <91967@VTEKURI>
2024-07-25 10:49:03 +0530

    Supported multiple students

--- demo/src/main/java/com/example/demo/HelloWorldSpringBoot.java
+++ demo/src/main/java/com/example/demo/HelloWorldSpringBoot.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +18,24 @@ public class HelloWorldSpringBoot {
 	}
 	
 	@GetMapping(value="/studentinfo")
-	public Student studentInfo() {
+	public List<Student> studentInfo() {
 		Student stu = new Student();
+		ArrayList<Student> students = new ArrayList<Student> ();
 		stu.setId(101);
 		stu.setName("Yuvan");
 		stu.setSection("Mango");
 		stu.setAddress("19, 1st floor, Gods Gift Layout,560049");
-		return stu;
+		stu.setSchoolName("Kidzee");
+		students.add(stu);
+	
+		Student stu1 = new Student();
+		stu1.setId(102);
+		stu1.setName("Yashika");
+		stu1.setSection("Mango");
+		stu1.setAddress("19, 1st floor, Gods Gift Layout,560049");
+		stu1.setSchoolName("Kidzee");
+		students.add(stu1);
+		return students;
 	}
 
 }
